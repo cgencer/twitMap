6cae3e62a8a2d79835f308b1b893a6c2ecde7fc0
Cem Gencer <cem.gencer@me.com>
2014-04-01 23:47:59 +0300

    html5 optimizations

--- source/index.js
+++ source/index.js
@@ -1,33 +1,26 @@
 var theApp = (function(theApp, undefined) {
 
-	var fs = require("fs"),
-		io = require('socket.io'),
-		http = require('http'),
-		express = require('express'),
-		stylus = require('stylus'),
-		nib = require('nib'),
-		axis = require('axis-css'),
-		compressor = require('node-minify'),
-		twitter = require('ntwitter'),
-		passport = require('passport'),
+	var fs = require("fs"),				cronJob = require('cron').CronJob,			_ = require('underscore'),
+		io = require('socket.io'),		http = require('http'),						express = require('express'),
+		stylus = require('stylus'),		fontFace = require('stylus-font-face'),		nib = require('nib'),
+		axis = require('axis-css'),		fluidity = require('fluidity'),				compressor = require('node-minify'),
+		twitter = require('ntwitter'),	passport = require('passport'),
 		FacebookStrategy = require('passport-facebook').Strategy,
-		cronJob = require('cron').CronJob,
-		_ = require('underscore'),
 		path = require('path');
-
+		
 	require('./routes');
 
-	var env = process.env.NODE_ENV || 'development',
-		cfg = require('./config')[env];
+	var env = process.env.NODE_ENV || 'development', cfg = require('./config')[env];
 
 		_.str = require('underscore.string');
 		_.mixin(_.str.exports()),
 		_.str.include('Underscore.string', 'string');
 
-	var app = express();
-	var compile = function(str, path) {
-		return stylus(str).set('filename', path).use( axis() ).use( nib() );
-	}
+	var app = express(),
+		server = http.createServer(app),
+		compile = function(str, path) {
+			return stylus(str).set('filename', path).use( axis() ).use( nib() ).use( fluidity() ).use( fontFace() );
+		};
 
 	app.configure('development', function () {app.use(express.errorHandler());});
 	var __dirName = __dirname;
@@ -44,11 +37,14 @@ var theApp = (function(theApp, undefined) {
 			debug: true,
 			force: true,
 			src: 	__dirName + '/style',
-			dest: 	__dirName + "/public/css/",
+			dest: 	__dirName + '/public/css/',
 			compile : function(str, path) {
 				console.log('compiling');
 				return stylus(str)
-					.use(nib())
+					.use( nib() )
+					.use( fontFace() )
+					.use( axis() )
+					.use( fluidity() )
 					.set('filename', path)
 					.set('warn', true)
 					.set('compress', true)
@@ -61,13 +57,23 @@ var theApp = (function(theApp, undefined) {
 		app.use( express.static(__dirName + '/public') );
 		app.use( express.errorHandler( { dumpExceptions: true, showStack: true } ) );
 	});
+/*
 	new compressor.minify({
 		type: 'clean-css',
-		fileIn: ['public/css/style.css'],
+		fileIn: ['public/css/reset.css', 'public/css/style.css'],
 		fileOut: 'public/css/style-min.css',
 		callback: function(err, min){
-			if(err)console.log(err);
+			if(err) console.log(err);
 		}
+	});
+*/
+	var sockets = io.listen(server);
+	sockets.configure(function() { //THIS IS NECESSARY ONLY FOR HEROKU!
+		sockets.set('transports', ['xhr-polling']);
+		sockets.set('polling duration', 10);
+	});
+	sockets.sockets.on('connection', function(socket) { 
+		socket.emit('data', watchList);
 	});
 
 	process.on('uncaughtException', function(err) {
